3283593a84e4efcb59530f6bada7937d7ede16cd
Sir Vick <11915081+sirv1ck@users.noreply.github.com>
2025-05-10 15:56:56 +0100

    Update demoMenuList.js

--- src/assets/data/demoMenuList.js
+++ src/assets/data/demoMenuList.js
@@ -1,43 +1,11 @@
 const Data = [
   {
-    title: "Demo One",
+    title: "Home",
     url: "/",
   },
   {
-    title: "Demo Two",
-    url: "/home-two",
-  },
-  {
-    title: "Demo Three",
-    url: "/home-three",
-  },
-  {
-    title: "Demo Four",
-    url: "/home-four",
-  },
-  {
-    title: "Demo Five",
-    url: "/home-five",
-  },
-  {
-    title: "Demo Six",
-    url: "/home-six",
-  },
-  {
-    title: "Demo Seven",
-    url: "/home-seven",
-  },
-  {
-    title: "Demo Eight",
-    url: "/home-eight",
-  },
-  {
-    title: "Demo Nine",
-    url: "/home-nine",
-  },
-  {
-    title: "Demo Ten",
-    url: "/home-ten",
+    title: "WhitePaper",
+    url: "/assets/whitepaper-Cii2spLT.pdf",
   },
 ];
 
